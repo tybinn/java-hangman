864e2f9558c855e6160883cbcefd73dbbe6023a7
tybin <tybinka@agh.edu.pl>
2020-03-01 10:25:17 +0100

    Importing class done!

--- src/main/java/pl/edu/agh/hangman/FileReader.java
+++ src/main/java/pl/edu/agh/hangman/FileReader.java
@@ -1,5 +1,22 @@
 package pl.edu.agh.hangman;
-
+import java.util.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.io.*;
 public class FileReader {
+    public static List<String> readFileInList(String fileName)
+    {
+        List<String> lines = Collections.emptyList();
+        try
+        {
+            lines = Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
+        }
 
+        catch (IOException e)
+        {
+            // do something
+            e.printStackTrace();
+        }
+        return lines;
+    }
 }
--- src/test/java/pl/edu/agh/hangman/HangmanTest.java
+++ src/test/java/pl/edu/agh/hangman/HangmanTest.java
@@ -3,9 +3,21 @@ package pl.edu.agh.hangman;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.List;
+
 public class HangmanTest {
     @Test
     public void testThatTestsNothing() {
         Assert.assertTrue("You need to implement it!", false);
+    }
+
+    @Test
+    public void testImportFile(){
+        FileReader fr = new FileReader();
+        List<String> words;
+
+        words = fr.readFileInList("C:\\Users\\student44\\hangman\\java-hangman\\src\\main\\resources\\slowa.txt");
+        Assert.assertEquals("file loaded",words.get(1), "prawnik");
+
     }
 }
