aa627ea70c5c0fab9c9eda1aaf6a32639c1f4ca0
tybin <tybinka@agh.edu.pl>
2020-03-01 11:10:26 +0100

    Game state - engine of the game will be soon

--- src/main/java/pl/edu/agh/hangman/GameState.java
+++ src/main/java/pl/edu/agh/hangman/GameState.java
@@ -2,9 +2,25 @@ package pl.edu.agh.hangman;
 
 public class GameState {
     private String word;
+    private String currentView;
     private int numOfAttempts;
+    private int badChoiceses;
     public GameState(String word){
         this.word = word;
+        currentView =  generateEmptyFields(word.length());
     }
+    public boolean isInWord(String letter){
+        if (word.contains(letter)){
 
+        }else{
+            badChoiceses =+ 1;
+        }
+    }
+    private String generateEmptyFields(int len){
+        String retVal = "";
+        for (int i =1; i <= len; i++ ){
+            retVal = retVal + "_";
+        }
+        return retVal;
+    }
 }
