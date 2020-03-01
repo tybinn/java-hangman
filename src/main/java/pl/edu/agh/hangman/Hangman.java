e07d9d446e323088d9381a23a338e5f1d6c3c949
martatomalska <tomalska.marta@gmail.com>
2020-03-01 11:07:58 +0100

    choose random element class added

--- src/main/java/pl/edu/agh/hangman/Hangman.java
+++ src/main/java/pl/edu/agh/hangman/Hangman.java
@@ -1,5 +1,9 @@
 package pl.edu.agh.hangman;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Hangman {
 
     public static final String[] HANGMANPICS = new String[]{
@@ -55,5 +59,7 @@ public class Hangman {
     };
 
     public static void main(String[] args) {
+
+        System.out.println("");
     }
 }
